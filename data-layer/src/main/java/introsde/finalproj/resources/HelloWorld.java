2b5a96013800ce931269dde11ad8273157e833c2
samuele.malavasi <samuele.malavasi@gmail.com>
2017-06-01 14:37:22 +0200

    configured each .pom to deploy on heroku AND configured 'data-layer' to interact with EC2 amazon instance with mongoDB

--- data-layer/src/main/java/introsde/finalproj/resources/HelloWorld.java
+++ data-layer/src/main/java/introsde/finalproj/resources/HelloWorld.java
@@ -1,10 +1,21 @@
 package introsde.finalproj.resources;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+
+import org.eclipse.persistence.internal.nosql.adapters.mongo.MongoJCAConnectionSpec;
+import org.eclipse.persistence.nosql.adapters.mongo.MongoConnectionSpec;
+
+import introsde.finalproj.model.User;
 @Path("/hello-world")
 public class HelloWorld {
 	
@@ -15,6 +26,7 @@ public class HelloWorld {
     
     @Produces({MediaType.APPLICATION_JSON})
     public Response helloWorld() {
+    	
     	return Response.status(Response.Status.OK).entity("{\"data\":\"hello world\"}").build();
     }      
     
--- sharedModels/src/main/java/introsde/finalproj/dao/MyDao.java
+++ sharedModels/src/main/java/introsde/finalproj/dao/MyDao.java
@@ -1,20 +1,27 @@
 package introsde.finalproj.dao;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
+import org.eclipse.persistence.internal.nosql.adapters.mongo.MongoJCAConnectionSpec;
+import org.eclipse.persistence.nosql.adapters.mongo.MongoConnectionSpec;
+
 public enum MyDao {
     instance;
     private EntityManagerFactory emf;
 
+    
     private MyDao() {
         if (emf!=null) {
             emf.close();
         }
         emf = Persistence.createEntityManagerFactory("introsde-final-jpa");
-    }
+    } 
 
     public EntityManager createEntityManager() {
         try {
